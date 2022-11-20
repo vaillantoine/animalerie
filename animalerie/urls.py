d5dfead11eb878af8658a786af6bcac3949b098c
Antoine VAILLANT <antoine.vaillant@centrale-marseille.fr>
2022-11-20 20:24:34 +0100

    animal_detail

--- animalerie/urls.py
+++ animalerie/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('animal/<str:id animal>/', views.animal_detail, name='animal_detail'),
+    path('animal/<str:id_animal>/?<str:message>', views.animal_detail, name='animal_detail_mes'),
 ]
--- animalerie/views.py
+++ animalerie/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Animal
+from .forms import MoveForm
 
 
 def home(request):
     animaux = Animal.objects.all()
 
     return render(request, 'animalerie/home.html', {'animaux': animaux})
+
+
+def animal_detail(request, id_animal):
+    animal = get_object_or_404(Animal, id_animal=id_animal)
+    lieu = animal.lieu
+    form = MoveForm()
+    return render(request,
+                  'animaleire/animalerie_detail.html',
+                  {'animal': animal, 'lieu': lieu, 'form': form})
