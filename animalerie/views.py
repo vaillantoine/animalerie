26ceafb85c5ec66b84ac0e545662885e1ad6504a
Antoine VAILLANT <antoine.vaillant@centrale-marseille.fr>
2022-11-21 11:27:47 +0100

    message alerte

--- animalerie/views.py
+++ animalerie/views.py
@@ -10,6 +10,7 @@ def animal_list(request):
 
 
 def animal_detail(request, id_animal):
+    message = ''
     animal = get_object_or_404(Animal, id_animal=id_animal)
     ancien_etat = animal.etat
     ancien_lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
@@ -23,21 +24,26 @@ def animal_detail(request, id_animal):
                        'roue': 'fatigué',
                        'nid': 'endormi'}
         nouveau_lieux_ancien_etat = {'mangeoire': 'affamé',
-                                    'roue': 'repus',
-                                    'nid': 'fatigué',
-                                    'litière': 'endormi'}
-        if (ancien_etat == nouveau_lieux_ancien_etat[nouveau_lieu.id_equip]) and (nouveau_lieu.disponibilite or (nouveau_lieu.id_equip == 'litière')):
+                                     'roue': 'repus',
+                                     'nid': 'fatigué',
+                                     'litière': 'endormi'}
+        if (ancien_etat == nouveau_lieux_ancien_etat[nouveau_lieu.id_equip]) and (
+                nouveau_lieu.disponibilite or (nouveau_lieu.id_equip == 'litière')):
             ancien_lieu.disponibilite = True
             ancien_lieu.save()
             nouveau_lieu.disponibilite = False
             nouveau_lieu.save()
             animal.etat = lieux_etats[nouveau_lieu.id_equip]
             animal.save()
-        return redirect('animal_detail', id_animal=id_animal)
-    else:
-        form = MoveForm()
-        nouveau_lieu = animal.lieu
+            return redirect('animal_detail', id_animal=id_animal)
+        else:
+            message = "Impossible ! Action interdite"
+    form = MoveForm()
+    lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
 
     return render(request,
                   'animalerie/animal_detail.html',
-                  {'animal': animal, 'lieu': nouveau_lieu, 'form': form})
+                  {'animal': animal,
+                   'lieu': lieu,
+                   'form': form,
+                   'message': message})
