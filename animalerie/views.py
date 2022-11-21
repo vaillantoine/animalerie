7a3be7283aa1579257ab54dc4e7ba4031ef24f13
Antoine VAILLANT <antoine.vaillant@centrale-marseille.fr>
2022-11-21 11:04:46 +0100

    changement lieu ok

--- animalerie/views.py
+++ animalerie/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Animal
+from .models import Animal, Equipement
 from .forms import MoveForm
 
 
@@ -11,8 +11,33 @@ def animal_list(request):
 
 def animal_detail(request, id_animal):
     animal = get_object_or_404(Animal, id_animal=id_animal)
-    lieu = animal.lieu
-    form = MoveForm()
+    ancien_etat = animal.etat
+    ancien_lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
+    form = MoveForm(request.POST, instance=animal)
+
+    if form.is_valid():
+        form.save(commit=False)
+        nouveau_lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
+        lieux_etats = {'litière': 'affamé',
+                       'mangeoire': 'repus',
+                       'roue': 'fatigué',
+                       'nid': 'endormi'}
+        nouveau_lieux_ancien_etat = {'mangeoire': 'affamé',
+                                    'roue': 'repus',
+                                    'nid': 'fatigué',
+                                    'litière': 'endormi'}
+        if (ancien_etat == nouveau_lieux_ancien_etat[nouveau_lieu.id_equip]) and (nouveau_lieu.disponibilite or (nouveau_lieu.id_equip == 'litière')):
+            ancien_lieu.disponibilite = True
+            ancien_lieu.save()
+            nouveau_lieu.disponibilite = False
+            nouveau_lieu.save()
+            animal.etat = lieux_etats[nouveau_lieu.id_equip]
+            animal.save()
+        return redirect('animal_detail', id_animal=id_animal)
+    else:
+        form = MoveForm()
+        nouveau_lieu = animal.lieu
+
     return render(request,
                   'animalerie/animal_detail.html',
-                  {'animal': animal, 'lieu': lieu, 'form': form})
+                  {'animal': animal, 'lieu': nouveau_lieu, 'form': form})
